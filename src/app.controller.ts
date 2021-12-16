95b4cb58625f16d85e9662e44c99348f55e75108
Spencer Easton <spencereaston@amplifiedit.com>
2021-12-15 20:44:43 -0500

    MVP Repo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { HealthCheck, HealthCheckService, HttpHealthIndicator } from "@nestjs/terminus";
+import {
+  HealthCheck,
+  HealthCheckService,
+  HttpHealthIndicator,
+} from '@nestjs/terminus';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService,
-              private health: HealthCheckService,
-              private http: HttpHealthIndicator,) {}
+  constructor(
+    private readonly appService: AppService,
+    private health: HealthCheckService,
+    private http: HttpHealthIndicator,
+  ) {}
 
   @Get()
   getHello(): string {
@@ -20,7 +26,4 @@ export class AppController {
       () => this.http.pingCheck('nestjs-docs', 'https:'),
     ]);
   }
-
-
-
 }

3dc74cf72ac5341d507d66ad3e99bf5dc017a52c
Spencer Easton <spencereaston@amplifiedit.com>
2021-12-15 20:43:02 -0500

    MVP Repo

---
+++ src/app.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { HealthCheck, HealthCheckService, HttpHealthIndicator } from "@nestjs/terminus";
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService,
+              private health: HealthCheckService,
+              private http: HttpHealthIndicator,) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('health')
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      () => this.http.pingCheck('nestjs-docs', 'https:'),
+    ]);
+  }
+
+
+
+}
---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TerminusModule } from "@nestjs/terminus";
+import { HttpModule } from "@nestjs/axios";
+
+@Module({
+  imports: [TerminusModule,HttpModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
